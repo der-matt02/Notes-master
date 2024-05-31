726a114133ed5f2b17802906a67e6b155177a58a
der-matt02 <dermatt02@gmail.com>
2024-05-30 21:31:06 -0500

    Agregar archivos de proyecto.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models;
+
+internal class About
+{
+    public string Nombre => "Mathew Baquero";
+    public string Descripcion => "Amante de la tecnologia y es fanatico de la progrmaacion en Python y es adipto al uso de hardware, contemplando el uso de microcontroladores y single board computers"
